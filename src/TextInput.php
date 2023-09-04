c04ed9e9ec73e6970500f8267de3f1a443eead9e
Thiago Ribeiro <t.r.goveia@gmail.com>
2023-09-04 11:42:50 -0300

    feat: add text input

---
+++ src/TextInput.php
@@ -0,0 +1,18 @@
+<?php
+class TextInput extends Input {
+
+    public function __construct($name, $label, $initVal) {
+        $this->_name = $name;
+        $this->_label = $label;
+        $this->_initVal = $initVal;
+    }
+
+    public function validate() {
+        return !empty($this->_initVal);
+    }
+
+    protected function _renderSetting() {
+        return '<input class="text-input" type="text" name="' . $this->_name . '" value="' . htmlspecialchars($this->_initVal) . '">';
+    }
+
+}
