7de967d875acb4f91548835e47a12495ce5022f9
Thiago Ribeiro <t.r.goveia@gmail.com>
2023-10-08 10:54:35 -0300

    feat: add form controller for separation of concerns between PHP and HTML logic

---
+++ src/FormController.php
@@ -0,0 +1,22 @@
+<?php
+class FormController {
+    private $form;
+
+    public function __construct(Form $form) {
+        $this->form = $form;
+    }
+
+    public function handleForm() {
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            if ($this->form->validate()) {
+                $firstName = $this->form->getValue("firstname");
+                $lastName = $this->form->getValue("lastname");
+                return "Hello, " . htmlspecialchars($firstName) . " " . htmlspecialchars($lastName);
+            } else {
+                return '<div class="alert alert-danger">Form is invalid</div>' . FormView::renderForm($this->form);
+            }
+        } else {
+            return $this->form->display();
+        }
+    }
+}
